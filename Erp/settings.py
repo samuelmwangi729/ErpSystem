162e6fbc97c80897486e69bb2faf48a23f5533c7
samuel mwangi <samuelmwangi729@gmail.com>
2021-05-24 05:01:00 +0300

    done

--- Erp/settings.py
+++ Erp/settings.py
@@ -132,4 +132,4 @@ STATICFILES_DIRS=[
 ]
 MEDIA_URL='/pictures/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'pictures')
-# STATIC_ROOT=os.path.join(BASE_DIR,'static')
+# STATIC_ROOT=os.path.join(BASE_DIR,'static')                                  
